2972e7ec9d6bd7af11af71f978cb37cb84fdc4b7
GitHub Action <action@github.com>
2021-04-09 12:02:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/zaazbb/intellijrsteditor/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zaazbb/intellijrsteditor/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zaazbb.intellijrsteditor.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zaazbb.intellijrsteditor.MyBundle
 
 class MyApplicationService {
 
